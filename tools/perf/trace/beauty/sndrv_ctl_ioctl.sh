f7f61cd63873f3c352b583244c18a07ab38e6b54
Michael Forney <mforney@mforney.org>
2021-04-06 22:27:45 -0700

    tools/perf: Improve portability of sed invocations
    Signed-off-by: Michael Forney <mforney@mforney.org>

--- tools/perf/trace/beauty/sndrv_ctl_ioctl.sh
+++ tools/perf/trace/beauty/sndrv_ctl_ioctl.sh
@@ -4,6 +4,6 @@
 [ $# -eq 1 ] && header_dir=$1 || header_dir=tools/include/uapi/sound/
 
 printf "static const char *sndrv_ctl_ioctl_cmds[] = {\n"
-grep "^#define[\t ]\+SNDRV_CTL_IOCTL_" $header_dir/asound.h | \
-	sed -r 's/^#define +SNDRV_CTL_IOCTL_([A-Z0-9_]+)[\t ]+_IO[RW]*\( *.U., *(0x[[:xdigit:]]+),?.*/\t[\2] = \"\1\",/g'
+grep "^#define[	 ]\+SNDRV_CTL_IOCTL_" $header_dir/asound.h | \
+	sed -E 's/^#define +SNDRV_CTL_IOCTL_([A-Z0-9_]+)[	 ]+_IO[RW]*\( *.U., *(0x[[:xdigit:]]+),?.*/	[\2] = \"\1\",/g'
 printf "};\n"
